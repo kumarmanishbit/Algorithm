79841dd4c7e2a596f696eced0daad2c643c75464
Manish Kumar <manish.br.kumar@oracle.com>
2022-02-25 19:37:43 +0530

    Adding recursive solution for minimum jumps to reach end

--- src/com/mani/soni/dynamicprogramming/MinimumJumpsToReachEnd.java
+++ src/com/mani/soni/dynamicprogramming/MinimumJumpsToReachEnd.java
@@ -116,7 +116,6 @@ public class MinimumJumpsToReachEnd {
      */
     public static int jump(int[] nums) {
 
-
         int dp[] = new int[nums.length];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
@@ -125,15 +124,11 @@ public class MinimumJumpsToReachEnd {
         // This is like LIC problem. We start looking backward.
         for(int i=0 ; i < nums.length; ++i) {
             for(int j = 0; j < i; j++) {
-
                 if(nums[j] + j >= i) {
                     dp[i] = Math.min(dp[i] , dp[j] + 1);
-
                 }
-
             }
         }
-
         return dp[nums.length - 1];
     }
 
