7e6e21e52e84d5f1f2cd63d323b6be12823080fe
Manish Kumar <manish.br.kumar@oracle.com>
2022-03-04 18:54:46 +0530

    longest substring with k unique character

--- src/com/mani/soni/slidingwindow/LargestSubarrayOfSumK.java
+++ src/com/mani/soni/slidingwindow/LargestSubarrayOfSumK.java
@@ -3,6 +3,32 @@ package com.mani.soni.slidingwindow;
 public class LargestSubarrayOfSumK {
 
     public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 7, 1, 9 };
+        int k = 15;
 
+        int j = 0, i = 0;
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        while(j < arr.length) {
+            sum += arr[j];
+
+            if(sum < k) {
+                j++;
+                continue;
+            }
+
+            if(sum == k) {
+                maxSum = Math.max(maxSum, j - i + 1);
+                j++;
+            } else if (sum > k){
+                while (sum > k && i < arr.length) {
+                  sum -= arr[i];
+                  i++;
+                }
+                j++;
+            }
+        }
+
+        System.out.println(maxSum);
     }
 }
---
+++ src/com/mani/soni/slidingwindow/LongestSubStringWithKUniqueCharacter.java
@@ -0,0 +1,45 @@
+package com.mani.soni.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubStringWithKUniqueCharacter {
+
+    public static void main(String[] args) {
+        int[] arr = {6, 5, 1, 2, 3, 2, 1, 4, 5};
+
+        int k = 3;
+
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        int j = 0;
+        int i = 0;
+
+        int maxUnique = Integer.MIN_VALUE;
+
+        while (j < arr.length) {
+
+            int freq = frequency.getOrDefault(arr[j], 0);
+            frequency.put(arr[j], freq + 1);
+
+            if(frequency.size() < k) {
+                maxUnique = Math.max(maxUnique, j - i + 1);
+                j++;
+            }else if(frequency.size() == k) {
+                maxUnique = Math.max(maxUnique, j - i + 1);
+                j++;
+            } else if(frequency.size() > k) {
+                while(frequency.size() > k) {
+                    int lastFrequency = frequency.get(arr[i]);
+                    if(lastFrequency == 0) {
+                        frequency.remove(arr[i]);
+                    } else {
+                        frequency.put(arr[i], lastFrequency - 1);
+                    }
+                }
+                i++;
+            }
+        }
+        System.out.println(maxUnique);
+    }
+}
