9d91cae095ebcb8239286ad9a821a9157571109e
Manish Kumar <manish.br.kumar@oracle.com>
2022-03-14 05:36:38 +0530

    Adding tree problem

--- src/com/mani/soni/tree/BinaryTreeToDoublyLinkedList.java
+++ src/com/mani/soni/tree/BinaryTreeToDoublyLinkedList.java
@@ -1,5 +1,12 @@
 package com.mani.soni.tree;
 
+/**
+ * we need to do the inorder traversal of the Tree and while doing inorder traversal
+ * we can simply maintain a previous pointer or reference of the previously traversed
+ * node. And change right child of the previous node to current node and left child
+ * of current node as previous
+ *
+ */
 public class BinaryTreeToDoublyLinkedList {
 
     public static void main(String[] args) {
